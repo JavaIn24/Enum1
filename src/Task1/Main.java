c967d70d08a122729a5c6bf6e3c0b64e8a54ebf8
JavaIn24 <yaracrut245@gmail.com>
2024-08-03 14:25:19 +0500

    Update Main.java

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,18 @@
-package Task1;public class Main {
+package Task1;
+public class Main {
+  public static void main(String[] args) {
+        printIndexAlphabet("Я");
+
+    }
+
+    enum Alphabet {
+        А, Б, В, Г, Д, Е, Ё, Ж, З, И,
+        Й, К, Л, М, Н, О, П, Р, С,
+        Т, У, Ф, Х, Ц, Ч, Ш, Щ, Ъ,
+        Ы, Ь, Э, Ю, Я
+    }
+
+    public static void printIndexAlphabet(String symbol) {
+        System.out.println(Main.Alphabet.valueOf(symbol).ordinal() + 1);
+    }
 }
