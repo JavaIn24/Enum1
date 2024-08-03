8da8b902f23ea53f111f21a4195722f4bbee4882
JavaIn24 <yaracrut245@gmail.com>
2024-08-03 14:25:54 +0500

    Update Person.java

--- src/Task2/Person.java
+++ src/Task2/Person.java
@@ -1,2 +1,27 @@
-package Task2;public class Person {
+package Task2;
+public class Person {
+   String name;
+    String age;
+    Gender gender;
+
+
+    public Person(String age, String name, Gender gender) {
+        this.age= age;
+        this.name = name;
+        this.gender = gender;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Член клуба читателей возрастом " +
+                age + ' ' +
+                ", по имени " + name + ' ' +
+                ", пол члена " + gender + '\n';
+    }
+
+    enum Gender{
+        MALE,
+        FEMALE
+    }
 }
